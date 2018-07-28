18b88edce755f30b6ff9a677bb6b1975da4a406e
yxkillz <yixuankillz@hotmail.com>
2018-07-28 13:28:19 +0800

    Implemented RFID card registration

--- check_card.py
+++ check_card.py
@@ -11,16 +11,19 @@ mfrc522 = MFRC522.MFRC522()
 
 # This loop keeps checking for chips.
 # If one is near it will get the UID
-
-while continue_reading:
-    
-    # Scan for cards    
-    (status,TagType) = mfrc522.MFRC522_Request(mfrc522.PICC_REQIDL)
-
-    # If a card is found
-    if status == mfrc522.MI_OK:
-        # Get the UID of the card
-        (status,uid) = mfrc522.MFRC522_Anticoll()
-        print("UID of card is {}".format(uid))
-	GPIO.cleanup()
-	sys.exit()
+def read():
+	print("Waiting for card")
+	while continue_reading:
+	    
+	    # Scan for cards    
+	    (status,TagType) = mfrc522.MFRC522_Request(mfrc522.PICC_REQIDL)
+	
+	    # If a card is found
+	    if status == mfrc522.MI_OK:
+	        # Get the UID of the card
+	        (status,id) = mfrc522.MFRC522_Anticoll()
+		uid = ''.join(str(i) for i in id )
+	        print("UID of card is {}".format(uid))
+		return uid
+		GPIO.cleanup()
+		sys.exit()
